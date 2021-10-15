3394ddb2415aae726fa2b6fa74d11bc7d9e6b7af
Jorge <joradiazf@gmail.com>
2021-10-15 00:03:22 -0500

    Nueva latoneria y pintura

--- src/main/java/com/tiendavirtual/dao/VentasDAO.java
+++ src/main/java/com/tiendavirtual/dao/VentasDAO.java
@@ -31,17 +31,23 @@ public class VentasDAO {
 		ConexionBD conex = new ConexionBD();
 
 		String sql = "";
+		String etiquetaCOl="";
+		String etiquetaFIL="";
 		if (tipo.trim().equals("producto")) {
 			sql = "SELECT codigo_producto_fk AS Item, SUM(cantidad) AS Unidades\r\n"
 					+ "FROM ventas\r\n"
 					+ "GROUP BY codigo_producto_fk\r\n"
 					+ "ORDER BY codigo_producto_fk;";
+			etiquetaCOl="Codigo del producto";
+			etiquetaFIL="Cantidad de productos comprados";
 			
 		} else if (tipo.trim().equals("cliente")){
 			sql = "SELECT cedula_cliente_fk AS Item, SUM(cantidad) AS Unidades\r\n"
 					+ "FROM ventas\r\n"
 					+ "GROUP BY cedula_cliente_fk\r\n"
 					+ "ORDER BY cedula_cliente_fk;";
+			etiquetaCOl="Cedula del cliente";
+			etiquetaFIL="Cantidad de productos comprados";
 		}
 
 		try {
